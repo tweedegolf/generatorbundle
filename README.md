2e0f8e2b27e2f39545c45200f29309436f041a2d
Ruben Nijveld <ruben@gewooniets.nl>
2013-04-09 09:18:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Okoa Generator Bundle
---
+++ src/Tg/OkoaGeneratorBundle/TgOkoaGeneratorBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tg\OkoaGeneratorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Okoa generator bundle
+ */
+class TgOkoaGeneratorBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+    }
+}
